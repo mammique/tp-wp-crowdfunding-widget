7dbcb29465e4029d4b8e8b353692e03eb8649d4c
Camille Harang <mammique@yooook.net>
2013-04-26 00:41:14 +0200

    i18n

--- traceparent.php
+++ traceparent.php
@@ -29,6 +29,7 @@ function tp_crowdfunding_load_widgets() {
 	register_widget('Traceparent_Crowdfunding_Widget');
     wp_register_style('tp_crowdfunding_widget_css', WP_PLUGIN_URL . '/tp-wp-crowdfunding-widget/traceparent.css');
     wp_enqueue_style('tp_crowdfunding_widget_css');
+    load_theme_textdomain('traceparent', __DIR__.'/', __DIR__.'/');
 }
 
 /**
@@ -46,13 +47,13 @@ class Traceparent_Crowdfunding_Widget extends WP_Widget {
 	function Traceparent_Crowdfunding_Widget() {
 		/* Widget settings. */
 		$widget_ops = array('classname' => 'tp-crowdfunding-widget', 'description' =>
-                            __("A crowdfunding widget, displaying a funding gauge integrated with Traceparent's API and a PayPal a donation button.", 'tp-crowdfunding-widget'));
+                            __("A crowdfunding widget, displaying a funding gauge integrated with Traceparent's API and a PayPal a donation button.", 'traceparent'));
 
 		/* Widget control settings. */
 		$control_ops = array('width' => 300, 'height' => 350, 'id_base' => 'tp-crowdfunding-widget');
 
 		/* Create the widget. */
-		$this->WP_Widget('tp-crowdfunding-widget', __('Traceparent Crowdfunding', 'tp-crowdfunding-widget'), $widget_ops, $control_ops);
+		$this->WP_Widget('tp-crowdfunding-widget', __('Traceparent Crowdfunding', 'traceparent'), $widget_ops, $control_ops);
 	}
 
 	/**
@@ -63,7 +64,6 @@ class Traceparent_Crowdfunding_Widget extends WP_Widget {
 		extract($args);
 
 		/* Our variables from the widget settings. */
-
         $tp_url        = $instance['url'];
         $tp_auth_token = $instance['auth_token'];
         $tp_auth_user  = $instance['auth_user'];
@@ -107,7 +107,9 @@ class Traceparent_Crowdfunding_Widget extends WP_Widget {
         //curl_setopt($ch, CURLOPT_HTTPHEADER, array("Host: www.sandbox.paypal.com"));
         $res = curl_exec($ch);
 
-        $error_el = '<p class="tp_feedback tp_error">Something went wrong, our staff is working on it, sorry for the inconvenience.</p>';
+        $error_el = '<p class="tp_feedback tp_error">'.
+                    __("Something went wrong, our staff is working on it, sorry for the inconvenience.", 'traceparent').
+                    '</p>';
 
         if(!$res) {
 
@@ -156,16 +158,22 @@ class Traceparent_Crowdfunding_Widget extends WP_Widget {
 
                 if($pp_result['receiver_email'] != $pp_email) {
 
-                    echo '<p class="tp_feedback tp_error">PayPal receiver email address mismatch.</p>';
+                    echo '<p class="tp_feedback tp_error">'.
+                         __("receiver email address mismatch.", 'traceparent').
+                         '</p>';
 
                 } else if($pp_result['mc_currency'] != $unit['slug']) {
 
-                    echo '<p class="tp_feedback tp_error">currency mismatch.</p>';
+                    echo '<p class="tp_feedback tp_error">'.
+                         __("currency mismatch.", 'traceparent').
+                         '</p>';
 
                 } else if(count($metadata_prev)) {
 
-                    echo '<p class="tp_feedback tp_error">Transaction already registered.</p>';
-                    echo '<script type="text/javascript">document.location="./";</script>';
+                    echo '<p class="tp_feedback tp_error">'.
+                         __("Transaction already registered.", 'traceparent').
+                         '</p>';
+//                    echo '<script type="text/javascript">document.location="./";</script>';
 
                 } else { // If not already registered.
                                
@@ -217,19 +225,34 @@ class Traceparent_Crowdfunding_Widget extends WP_Widget {
                                    ->addPostFields(array('uuid' => $quantity['uuid']))->send();
 
                     wp_mail($pp_result['payer_email'],
-                            '['.get_bloginfo('name').'] Thank you for your participation!',
-                            "Hello ".$user['name'].", thank you for your participation to our project!\n\n".
-                            "You can check information about your payment on your PayPal account at https://paypal.com/\n\n".
-                            "Tracability and transparency about the use of your money is monitored ".
-                            "by the free and Open Source software Traceparent (http://traceparent.com/) ".
-                            "that is running the funding gauge on our website.\n\n".
-                            "Your profile information is managed by Gravatar, ".
-                            "if you want to set up your avatar please set it using this very same email (".
-                            $pp_result['payer_email'].") at http://gravatar.com/\n\n".
-                            "Best regards,\n\n".get_bloginfo('name').'.');
 
-                    echo '<p class="tp_feedback">Thank you!<small>Your participation has been succesfuly registered, '.
-                     'please check your email for more details.</small></p>';
+                            '['.get_bloginfo('name').'] '.__('Thank you for your participation!', 'traceparent'),
+
+                            __("Hello", 'traceparent').' '.$user['name'].", ".
+                            __("thank you for your participation to our project!", 'traceparent').
+                            "\n\n".
+                            __("You can check information about your payment on your PayPal account at https://paypal.com/", 'traceparent').
+                            "\n\n".
+                            __("Tracability and transparency about the use of your money is monitored ".
+                               "by the free and Open Source software Traceparent (http://traceparent.com/) ".
+                               "that is running the funding gauge on our website.", 'traceparent').
+                            "\n\n".
+                            sprintf(
+                                __("Your profile information is managed by Gravatar, ".
+                                   "if you want to set your avatar up, please set it using this very same email (%1$s) ".
+                                   "at http://gravatar.com/", 'traceparent'),
+                                $pp_result['payer_email']).
+                            "\n\n".
+                            sprintf(__"Best regards,", 'traceparent').
+                            "\n\n".
+                            get_bloginfo('name').'.');
+
+                    echo '<p class="tp_feedback">'.__("Thank you!", 'traceparent').
+                         '<small>'.
+                         __("Your participation has been succesfuly registered", 'traceparent').
+                         ', '.
+                         __("please check your email for more details.", 'traceparent').
+                         '</small></p>';
 
                     $request = $tp_client->get("metadata/snippet/filter/");
                     $request->getQuery()->set('assigned_counters', $tp_counter);
@@ -472,66 +495,66 @@ $.getJSON(tp_url + "/value/quantity/filter/",
 		$defaults = array('url' => 'http://sandbox.api.traceparent.com/0.1-beta', 'auth_token' => '', 'auth_user' => '',
                           'scope' => '', 'unit' => '122cc224-7572-11e2-adfe-78929c525f0e',
                           'counter' => '', 'pp_url' => 'https://www.sandbox.paypal.com', 'pp_auth_token' => '',
-                          'pp_email' => '', 'pp_item_name' => 'Donation', 'pp_button' => 'Donate!');
+                          'pp_email' => '', 'pp_item_name' => 'Donation', 'pp_button' => __('Donate!', 'traceparent'));
 		$instance = wp_parse_args((array) $instance, $defaults); ?>
 
 		<p>
-			<label for="<?php echo $this->get_field_id('url'); ?>"><?php _e('url:', 'tp-crowdfunding-widget'); ?></label>
+			<label for="<?php echo $this->get_field_id('url'); ?>"><?php _e('url:', 'traceparent'); ?></label>
 			<input id="<?php echo $this->get_field_id('url'); ?>" name="<?php echo $this->get_field_name('url'); ?>" value="<?php echo $instance['url']; ?>" style="width: 100%;" />
 		</p>
 
 		<p>
-			<label for="<?php echo $this->get_field_id('auth_token'); ?>"><?php _e('auth_token:', 'tp-crowdfunding-widget'); ?></label>
+			<label for="<?php echo $this->get_field_id('auth_token'); ?>"><?php _e('auth_token:', 'traceparent'); ?></label>
 			<input id="<?php echo $this->get_field_id('auth_token'); ?>" name="<?php echo $this->get_field_name('auth_token'); ?>" value="<?php echo $instance['auth_token']; ?>" style="width: 100%;" />
 		</p>
 
 		<p>
-			<label for="<?php echo $this->get_field_id('auth_user'); ?>"><?php _e('auth_user:', 'tp-crowdfunding-widget'); ?></label>
+			<label for="<?php echo $this->get_field_id('auth_user'); ?>"><?php _e('auth_user:', 'traceparent'); ?></label>
 			<input id="<?php echo $this->get_field_id('auth_user'); ?>" name="<?php echo $this->get_field_name('auth_user'); ?>" value="<?php echo $instance['auth_user']; ?>" style="width: 100%;" />
 		</p>
 
 		<p>
-			<label for="<?php echo $this->get_field_id('scope'); ?>"><?php _e('scope:', 'tp-crowdfunding-widget'); ?></label>
+			<label for="<?php echo $this->get_field_id('scope'); ?>"><?php _e('scope:', 'traceparent'); ?></label>
 			<input id="<?php echo $this->get_field_id('scope'); ?>" name="<?php echo $this->get_field_name('scope'); ?>" value="<?php echo $instance['scope']; ?>" style="width: 100%;" />
 		</p>
 
 		<p>
-			<label for="<?php echo $this->get_field_id('unit'); ?>"><?php _e('unit:', 'tp-crowdfunding-widget'); ?></label>
+			<label for="<?php echo $this->get_field_id('unit'); ?>"><?php _e('unit:', 'traceparent'); ?></label>
 			<input id="<?php echo $this->get_field_id('unit'); ?>" name="<?php echo $this->get_field_name('unit'); ?>" value="<?php echo $instance['unit']; ?>" style="width: 100%;" />
 		</p>
 
 <!--		<p>
-			<label for="<?php echo $this->get_field_id('unit_pp_code'); ?>"><?php _e('unit_pp_code:', 'tp-crowdfunding-widget'); ?></label>
+			<label for="<?php echo $this->get_field_id('unit_pp_code'); ?>"><?php _e('unit_pp_code:', 'traceparent'); ?></label>
 			<input id="<?php echo $this->get_field_id('unit_pp_code'); ?>" name="<?php echo $this->get_field_name('unit_pp_code'); ?>" value="<?php echo $instance['unit_pp_code']; ?>" style="width: 100%;" />
 		</p> -->
 
 		<p>
-			<label for="<?php echo $this->get_field_id('counter'); ?>"><?php _e('counter:', 'tp-crowdfunding-widget'); ?></label>
+			<label for="<?php echo $this->get_field_id('counter'); ?>"><?php _e('counter:', 'traceparent'); ?></label>
 			<input id="<?php echo $this->get_field_id('counter'); ?>" name="<?php echo $this->get_field_name('counter'); ?>" value="<?php echo $instance['counter']; ?>" style="width: 100%;" />
 		</p>
 
 		<p>
-			<label for="<?php echo $this->get_field_id('pp_url'); ?>"><?php _e('pp_url:', 'tp-crowdfunding-widget'); ?></label>
+			<label for="<?php echo $this->get_field_id('pp_url'); ?>"><?php _e('pp_url:', 'traceparent'); ?></label>
 			<input id="<?php echo $this->get_field_id('pp_url'); ?>" name="<?php echo $this->get_field_name('pp_url'); ?>" value="<?php echo $instance['pp_url']; ?>" style="width: 100%;" />
 		</p>
 
 		<p>
-			<label for="<?php echo $this->get_field_id('pp_auth_token'); ?>"><?php _e('pp_auth_token:', 'tp-crowdfunding-widget'); ?></label>
+			<label for="<?php echo $this->get_field_id('pp_auth_token'); ?>"><?php _e('pp_auth_token:', 'traceparent'); ?></label>
 			<input id="<?php echo $this->get_field_id('pp_auth_token'); ?>" name="<?php echo $this->get_field_name('pp_auth_token'); ?>" value="<?php echo $instance['pp_auth_token']; ?>" style="width: 100%;" />
 		</p>
 
 		<p>
-			<label for="<?php echo $this->get_field_id('pp_email'); ?>"><?php _e('pp_email:', 'tp-crowdfunding-widget'); ?></label>
+			<label for="<?php echo $this->get_field_id('pp_email'); ?>"><?php _e('pp_email:', 'traceparent'); ?></label>
 			<input id="<?php echo $this->get_field_id('pp_email'); ?>" name="<?php echo $this->get_field_name('pp_email'); ?>" value="<?php echo $instance['pp_email']; ?>" style="width: 100%;" />
 		</p>
 
 		<p>
-			<label for="<?php echo $this->get_field_id('pp_item_name'); ?>"><?php _e('pp_item_name:', 'tp-crowdfunding-widget'); ?></label>
+			<label for="<?php echo $this->get_field_id('pp_item_name'); ?>"><?php _e('pp_item_name:', 'traceparent'); ?></label>
 			<input id="<?php echo $this->get_field_id('pp_item_name'); ?>" name="<?php echo $this->get_field_name('pp_item_name'); ?>" value="<?php echo $instance['pp_item_name']; ?>" style="width: 100%;" />
 		</p>
 
 		<p>
-			<label for="<?php echo $this->get_field_id('pp_button'); ?>"><?php _e('pp_button:', 'tp-crowdfunding-widget'); ?></label>
+			<label for="<?php echo $this->get_field_id('pp_button'); ?>"><?php _e('pp_button:', 'traceparent'); ?></label>
 			<input id="<?php echo $this->get_field_id('pp_button'); ?>" name="<?php echo $this->get_field_name('pp_button'); ?>" value="<?php echo $instance['pp_button']; ?>" style="width: 100%;" />
 		</p>
 
